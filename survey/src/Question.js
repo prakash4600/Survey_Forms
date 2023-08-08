14e88b1191c2343acebe33dffc85b2bb8a22fbbd
print-ashish <as727243@gmail.com>
2023-08-08 16:41:13 +0530

    jai shree ram

---
+++ survey/src/Question.js
@@ -0,0 +1,147 @@
+// const questionsAndAnswers = [
+//     {
+//       question: "Have you used our service before?",
+//       options: ["Paris", "London", "Berlin", "Rome"],
+//       answer: "Paris"
+//     },
+//     {
+//       question: "Which programming language is commonly used for web development?",
+//       options: ["Java", "Python", "JavaScript", "C++"],
+//       answer: "JavaScript"
+//     },
+//     {
+//       question: "What is the largest planet in our solar system?",
+//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
+//       answer: "Jupiter"
+//     },
+//     {
+//       question: "Who is the author of the Harry Potter book series?",
+//       options: ["J.R.R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Stephen King"],
+//       answer: "J.K. Rowling"
+//     },
+//     {
+//       question: "Which country is famous for the Taj Mahal?",
+//       options: ["India", "China", "Egypt", "Brazil"],
+//       answer: "India"
+//     },
+//     {
+//       question: "What does HTML stand for?",
+//       options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hypertext Transfer Protocol"],
+//       answer: "Hyper Text Markup Language"
+//     },
+//     {
+//       question: "What is the chemical symbol for water?",
+//       options: ["O", "H2O", "W", "H2"],
+//       answer: "H2O"
+//     },
+//     {
+//       question: "Which planet is known as the 'Red Planet'?",
+//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
+//       answer: "Mars"
+//     },
+//     {
+//       question: "What is the main function of the CPU in a computer?",
+//       options: ["Display images on the screen", "Store data", "Perform calculations", "Connect to the internet"],
+//       answer: "Perform calculations"
+//     },
+//     {
+//       question: "What is the largest mammal on Earth?",
+//       options: ["Elephant",  "Blue Whale", "Giraffe", "Lion"],
+//       answer: "Blue Whale"
+//     }
+//   ];
+  
+const questionsAndAnswers = [
+  {
+    question: "Have you used our service before?",
+    options: [
+      "Yes, regularly",
+      "Yes, occasionally",
+      "No, familiar with it",
+      "No, never heard of it"
+    ]
+  },
+  {
+    question: "On a scale of 1 to 10, how satisfied are you with our service?",
+    options: [
+      "1 (Very Dissatisfied)",
+      "4 (Neutral)",
+      "7",
+      "10 (Very Satisfied)"
+    ]
+  },
+  {
+    question: "What aspects of our service do you find most valuable? (Select one)",
+    options: [
+      "Convenience",
+      "Quality",
+      "Customer Service",
+      "Affordability"
+    ]
+  },
+  {
+    question: "What improvements or changes would enhance your experience with our service?",
+    options: [
+      "Faster service",
+      "More variety",
+      "Better communication",
+      "Other (please specify)"
+    ]
+  },
+  {
+    question: "How often do you use our service?",
+    options: [
+      "Daily",
+      "Weekly",
+      "Monthly",
+      "Rarely"
+    ]
+  },
+  {
+    question: "How likely are you to recommend our service to friends or family?",
+    options: [
+      "Extremely Likely",
+      "Likely",
+      "Unlikely",
+      "Extremely Unlikely"
+    ]
+  },
+  {
+    question: "What influenced your decision to choose our service over alternatives?",
+    options: [
+      "Reputation",
+      "Pricing",
+      "Features",
+      "Other (please specify)"
+    ]
+  },
+  {
+    question: "Have you encountered any issues while using our service?",
+    options: [
+      "Yes",
+      "No",
+      "Sometimes",
+      "very often"
+    ]
+  },
+  {
+    question: "If you answered 'Yes' to the previous question, please describe the issues you've encountered.",
+    options: [
+      "Technical glitches",
+      "Poor customer service",
+      "Billing problems",
+      "Other (please specify)"
+    ]
+  },
+  {
+    question: "Would you be interested in participating in future user testing or providing additional feedback?",
+    options: [
+      "Yes",
+      "No",
+      "maybe",
+      "never ever"
+    ]
+  }
+];
+
+  export default questionsAndAnswers;
